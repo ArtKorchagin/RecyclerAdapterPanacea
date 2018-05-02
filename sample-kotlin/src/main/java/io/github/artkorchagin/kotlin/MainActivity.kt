8b0f0322299ccdad62c358a111971bb1f016d385
Arthur Korchagin <lliepmah@yandex.ru>
2018-05-02 16:38:57 +0300

    Fixed library dependency

--- sample-kotlin/src/main/java/io/github/artkorchagin/kotlin/MainActivity.kt
+++ sample-kotlin/src/main/java/io/github/artkorchagin/kotlin/MainActivity.kt
@@ -1,12 +1,14 @@
 package io.github.artkorchagin.kotlin
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ac_main)
+        var mRX: RecyclerView? = null
     }
 }
